22e0d6320bb8ab5204c13a9704c9b5d12b1592a3
Subhanisks <40215886+Subhanisks@users.noreply.github.com>
2021-01-09 17:45:24 +0530

    Update hello-flask.py

--- hello-flask.py
+++ hello-flask.py
@@ -2,8 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+csrf = CSRFProtect()
+csrf.init_app(app)
+
 @app.route('/')
 def index():
     return 'Web App with Python Flask!'
 
-app.run(host='0.0.0.0', port=81)
+app.run(host='0.0.0.0', port=80)
